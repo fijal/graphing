299b66f091c9b6c71236f0e2a3f35173bb473eca
Maciej Fijalkowski <fijall@gmail.com>
2023-03-17 12:46:26 +0200

    try now

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function plot(r, target, title) {
-    MG.data_graphic({
-        title: title,
-        data: r,
-        x_accessor: 'date',
-        y_accessor: 'value',
-        target: target,
-//        interpolate: d3.curveLinear,
-        right: 40,
-        width: 800,
-        brush: 'xy',
-        height: 400
-    });
+    console.log(r.map(a => a[0]));
+    new Chart("stats", {
+      type: "line",
+      data: {
+        labels: r.map(a => a[0]),
+    datasets: [{
+      backgroundColor: "rgba(0,0,0,1.0)",
+      borderColor: "rgba(0,0,0,0.1)",
+      data: r.map(a => a[1])
+    }]},
+});
 }
 
 
@@ -19,7 +18,7 @@ $(document).ready(function()
     if (data_input == null) {
         $(document).html("You need to have data.js which has data_input = [... 2-element arrays corresponding to date and value ...]")
     } else {
-        var data = [data_input.map((elem => ({ date: new Date(elem[0]), value: elem[1] })))];
-        plot(data, "#stats", "performance")
+        var data = data_input;
+        plot(data, "stats", "performance");
     }
 });
